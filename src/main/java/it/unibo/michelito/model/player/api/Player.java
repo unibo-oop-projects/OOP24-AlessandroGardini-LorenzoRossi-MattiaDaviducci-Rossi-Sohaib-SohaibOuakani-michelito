c0338b752212634d38dd9170aec0109244c8b578
Sohaib <sohaib.ouakani@studio.unibo.it>
2025-02-04 21:55:11 +0100

    Palyer Command Interface added

---
+++ src/main/java/it/unibo/michelito/model/player/api/Player.java
@@ -0,0 +1,19 @@
+package it.unibo.michelito.model.player.api;
+
+import it.unibo.michelito.model.maze.api.Maze;
+import it.unibo.michelito.model.modelutil.Updatable;
+
+/**
+ * interface for Player.
+ */
+public interface Player extends Updatable {
+
+    /**
+     * set the next command of the player.
+     * @param command is the command to be executed.
+     */
+    void setCommand(PlayerCommand command);
+
+    @Override
+    void update(long time, Maze maze);
+}
---
+++ src/main/java/it/unibo/michelito/model/player/api/PlayerCommand.java
@@ -0,0 +1,9 @@
+package it.unibo.michelito.model.player.api;
+
+public interface PlayerCommand {
+
+    /*
+     * 
+     */
+    void execute (Player player);
+}
