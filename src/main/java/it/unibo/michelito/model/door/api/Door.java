1c24c3d8e93d985b5e9ebcc94dcc11ff484e22dc
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-06 19:30:01 +0100

    changes for checkstyle in door

--- src/main/java/it/unibo/michelito/model/door/api/Door.java
+++ src/main/java/it/unibo/michelito/model/door/api/Door.java
@@ -2,6 +2,15 @@ package it.unibo.michelito.model.door.api;
 
 import it.unibo.michelito.model.modelutil.Updatable;
 
+/**
+ * Interface for Door.
+ * Every implementation when updated should check if the Player has entered.
+ */
 public interface Door extends Updatable {
+    /**
+     * Getter of the door status.
+     *
+     * @return true if the door is open.
+     */
     boolean isOpen();
 }
