e9aaac1d0c209dafff19a751a42a6858b7a36da1
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-06 16:46:27 +0100

    add GameManager javadoc interface

--- src/main/java/it/unibo/michelito/model/gamemanager/api/GameManager.java
+++ src/main/java/it/unibo/michelito/model/gamemanager/api/GameManager.java
@@ -1,17 +1,25 @@
 package it.unibo.michelito.model.gamemanager.api;
 
 import it.unibo.michelito.model.player.api.PlayerCommand;
+import it.unibo.michelito.model.player.api.Player;
 
 /**
  * This interface models the GameManager of the Michelito Application.
  * This is the single entry point of the Model.
  */
 public interface GameManager {
+    /**
+     * Set the current command to {@link Player}.
+     * If a Command is present this method should be called before update.
+     *
+     * @param playerCommand the command to be executed on the {@link Player}.
+     */
     void setCommand(PlayerCommand playerCommand);
 
-    //void setCommand(PlayerCommand bombCommand); //TODO: when commandBomb is created
-
+    /**
+     * Update the state of the game.
+     *
+     * @param currentTime is the current currentTime in millisecond.
+     */
     void update(long currentTime);
-
-    void init();
 }
--- src/main/java/it/unibo/michelito/model/gamemanager/impl/GameManagerImpl.java
+++ src/main/java/it/unibo/michelito/model/gamemanager/impl/GameManagerImpl.java
@@ -3,33 +3,72 @@ package it.unibo.michelito.model.gamemanager.impl;
 import it.unibo.michelito.model.gamegenerator.api.GameGenerator;
 import it.unibo.michelito.model.gamemanager.api.GameManager;
 import it.unibo.michelito.model.maze.api.Maze;
-import it.unibo.michelito.model.maze.impl.MazeImpl;
 import it.unibo.michelito.model.player.api.PlayerCommand;
 
 public class GameManagerImpl implements GameManager {
+    private final static int MAX_MAZE_INDEX = 1;
+    private final static int STARTING_LIFE_COUNT = 5;
+
     private int currentLevel;
-    private int life;
+    private int currentLifeCount;
     private Maze currentMaze;
-    private GameGenerator gameGenerator; //TODO: insert constructor when provided -> can be final !!
 
-    @Override
-    public void setCommand(PlayerCommand playerCommand) {
+    private PlayerCommand playerCommand;
 
-    }
+    private final Runnable onGameWin;
+    private final Runnable onGameOver;
 
-    @Override
-    public void update(long currentTime) {
-        currentMaze.getUpdatable().forEach(e -> e.update(currentTime, currentMaze));
-
-    }
-
-    @Override
-    public void init() {
+    public GameManagerImpl(final Runnable onGameWin, final Runnable onGameOver) {
+        this.currentLifeCount = STARTING_LIFE_COUNT;
         this.currentLevel = 0;
-        currentMaze = new MazeImpl(gameGenerator.generate(this.currentLevel), this::loseLife);
+        this.onGameWin = onGameWin;
+        this.onGameOver = onGameOver;
+        this.startGame();
     }
 
-    private void loseLife(){
+    @Override
+    public void setCommand(final PlayerCommand playerCommand) {
+        this.playerCommand = playerCommand;
+    }
 
+    @Override
+    public void update(final long currentTime) {
+        this.consumePlayerCommand();
+        this.currentMaze.getUpdatable().forEach(e -> e.update(currentTime, this.currentMaze));
+    }
+    
+    private void startGame() {
+        this.currentLevel = 0;
+        this.currentMaze = this.generateMaze(this.currentLevel);
+    }
+    
+    private void loseLife(){
+        if (this.currentLifeCount == 0) {
+            this.onGameOver.run();
+        } else {
+            this.currentLifeCount--;
+            this.currentMaze = this.generateMaze(this.currentLevel);
+        }
+    }
+
+    private void WonMaze() {
+        if (this.currentLevel + 1 > MAX_MAZE_INDEX) {
+            this.onGameWin.run();
+        } else {
+            this.currentLevel++;
+            this.currentMaze = this.generateMaze(this.currentLevel);
+        }
+    }
+    
+    private Maze generateMaze(final int level) { //TODO: gamegenerate impl
+        GameGenerator gameGenerator; // new gameGeneratorImpl();
+        return null; //new MazeImpl(gameGenerator.generate(level), this::loseLife, this::WonMaze);
+    }
+    
+    private void consumePlayerCommand() {
+        if (this.playerCommand != null) {
+            playerCommand.execute(this.currentMaze.getPlayer());
+            playerCommand = null;
+        }
     }
 }
