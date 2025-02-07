bae4544179bc8e7bb6bb362ea35effd8862ad4b8
Mattia Daviducci <mattia.daviducci@studio.unibo.it>
2025-02-07 18:29:47 +0100

    fix BombFactoryImpl imports

--- src/main/java/it/unibo/michelito/model/bomb/impl/BombFactoryImpl.java
+++ src/main/java/it/unibo/michelito/model/bomb/impl/BombFactoryImpl.java
@@ -2,10 +2,7 @@ package it.unibo.michelito.model.bomb.impl;
 
 import it.unibo.michelito.model.bomb.api.Bomb;
 import it.unibo.michelito.model.bomb.api.BombFactory;
-import it.unibo.michelito.model.maze.api.Maze;
 import it.unibo.michelito.util.Position;
-import it.unibo.michelito.util.Type;
-import it.unibo.michelito.util.hitbox.api.HitBox;
 
 public class BombFactoryImpl implements BombFactory {
 
