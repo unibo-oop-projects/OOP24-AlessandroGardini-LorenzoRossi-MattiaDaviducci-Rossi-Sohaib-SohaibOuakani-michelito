4045b0ca0b0d4eb669a08ebb4e7d0812c29ef1a7
Lorenzo Rossi <lorenzo.rossi50@studio.unibo.it>
2025-02-07 22:20:06 +0100

    GameGenerator non interface anymore, now class for static purpose

--- src/main/java/it/unibo/michelito/model/gamegenerator/api/GameGenerator.java
+++ src/main/java/it/unibo/michelito/model/gamegenerator/api/GameGenerator.java
@@ -1,22 +1,57 @@
 package it.unibo.michelito.model.gamegenerator.api;
 
 import it.unibo.michelito.model.modelutil.MazeObject;
+import it.unibo.michelito.util.GameObject;
+import it.unibo.michelito.util.ObjectType;
+import it.unibo.michelito.util.Position;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  *
  */
-public interface GameGenerator {
+public class GameGenerator {
+    private static final Integer MAZEBLOCKWHID = 20;
+    private static final Integer MAZEBLOCKHIGHT = 15;
+    private Integer levelNumber;
+
+
+    /**
+     * @return
+     */
+     public GameGenerator() {
+         this.levelNumber = -1;
+     }
+
     /**
      * @param levelNumber
      * @return
      */
-    Set<MazeObject> generate(Integer levelNumber);
+     public static Set<GameObject> generate(Integer levelNumber){
+         Set<GameObject> maze = new HashSet<>();
+         for(int x = 0; x < MAZEBLOCKWHID; x++){
+             for(int y = 0; y < MAZEBLOCKHIGHT; y++){
+
+             }
+         }
+         for(int x = 0; x < MAZEBLOCKWHID; x++){
+             maze.add(new GameObject(ObjectType.WALL, new Position(x*6, 0)));
+             maze.add(new GameObject(ObjectType.WALL, new Position(x*6, MAZEBLOCKHIGHT*6)));
+         }
+         for(int x = 0; x < MAZEBLOCKHIGHT; x++){
+             maze.add(new GameObject(ObjectType.WALL, new Position(0, x*6)));
+             maze.add(new GameObject(ObjectType.WALL, new Position(MAZEBLOCKWHID*6, x*6)));
+         }
+
+         return new HashSet<>();
+    }
 
     /**
      *
      * @return
      */
-    Integer getLevelNumber();
+    Integer getLevelNumber(){
+        return this.levelNumber;
+    }
 }
--- src/main/java/it/unibo/michelito/model/gamegenerator/impl/GameGeneratorImpl.java
+++ src/main/java/it/unibo/michelito/model/gamegenerator/impl/GameGeneratorImpl.java
@@ -2,22 +2,23 @@ package it.unibo.michelito.model.gamegenerator.impl;
 
 import it.unibo.michelito.model.gamegenerator.api.GameGenerator;
 import it.unibo.michelito.model.modelutil.MazeObject;
+import it.unibo.michelito.util.GameObject;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class GameGeneratorImpl implements GameGenerator {
+public class GameGeneratorImpl {
     private int currentlevel;
 
-    @Override
-    public Set<MazeObject> generate(Integer levelNumber) {
+
+    public Set<GameObject> generate(Integer levelNumber) {
         this.currentlevel = levelNumber;
-        Set<MazeObject> mazeObjects = new HashSet<>();
+        Set<GameObject> mazeObjects = new HashSet<>();
         return mazeObjects;
 
     }
 
-    @Override
+
     public Integer getLevelNumber() {
         return currentlevel;
     }
