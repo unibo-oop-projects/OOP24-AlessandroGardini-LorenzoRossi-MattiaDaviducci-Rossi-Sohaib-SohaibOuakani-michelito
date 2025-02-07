a082c3bf2f9751f3deecf2b9d602d867095f5230
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-07 10:45:06 +0100

    add test implementation for door

--- src/test/java/it/unibo/michelito/model/door/TestDoor.java
+++ src/test/java/it/unibo/michelito/model/door/TestDoor.java
@@ -1,4 +1,47 @@
 package it.unibo.michelito.model.door;
 
-public class TestDoor {
+import it.unibo.michelito.model.door.api.Door;
+import it.unibo.michelito.model.door.impl.DoorImpl;
+import it.unibo.michelito.util.hitbox.api.HitBox;
+import it.unibo.michelito.util.Position;
+import it.unibo.michelito.util.hitbox.impl.HitBoxFactoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Test class for {@link DoorImpl}.
+ */
+final class TestDoor {
+    private Door door;
+    private Position position;
+
+    /**
+     * Creates a Door to be used in each test.
+     */
+    @BeforeEach
+    void setUp() {
+        position = new Position(0, 0);
+        door = new DoorImpl(position);
+    }
+
+    /**
+     * Tests {@link Position} and {@link HitBox}.
+     */
+    @Test
+    void testPosition() {
+        assertNotNull(door);
+        assertEquals(door.getHitBox(), new HitBoxFactoryImpl().squareHitBox(position));
+        assertEquals(door.position(), position);
+    }
+
+    /**
+     * Test opening.
+     */
+    @Test
+    void testOpening(){
+        //TODO: when enemy has constructor.
+    }
 }
