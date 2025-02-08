387bcee81302cdd7e3cf2c8f7007731565322e7b
Mattia Daviducci <mattia.daviducci@studio.unibo.it>
2025-02-08 01:13:15 +0100

    added BombType and changed parameters in createBomb

--- src/main/java/it/unibo/michelito/model/bomb/api/Bomb.java
+++ src/main/java/it/unibo/michelito/model/bomb/api/Bomb.java
@@ -4,16 +4,4 @@ import it.unibo.michelito.model.modelutil.Temporary;
 import it.unibo.michelito.model.modelutil.Updatable;
 
 public interface Bomb extends Temporary, Updatable {
-
-    void startTimer();
-
-    boolean isExploded();
-
-    void explode();
-
-    boolean isPassThrough();
-
-    int getRange();
-
-    // qualcosa generateFlame
 }
