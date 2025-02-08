16e9f1b996e8ae62ccb362977dc8f518e20bf0c7
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-08 16:26:33 +0100

    implementation of ObjectAdapterImpl and javadoc add to both the interface and impl

--- src/main/java/it/unibo/michelito/controller/objectsadapter/api/ObjectsAdapter.java
+++ src/main/java/it/unibo/michelito/controller/objectsadapter/api/ObjectsAdapter.java
@@ -4,6 +4,16 @@ import it.unibo.michelito.model.modelutil.MazeObject;
 
 import java.util.Set;
 
+/**
+ * Interface for an ObjectsAdapter.
+ * Adapter for the LevelGenerator.
+ */
 public interface ObjectsAdapter {
+    /**
+     * This method retrieves a set of {@link MazeObject} from the LevelGenerator.
+     *
+     * @param level the level to generate the set of maze objects.
+     * @return a set of {@link MazeObject} representing the level.
+     */
     Set<MazeObject> requestMazeObjects(final int level);
 }
