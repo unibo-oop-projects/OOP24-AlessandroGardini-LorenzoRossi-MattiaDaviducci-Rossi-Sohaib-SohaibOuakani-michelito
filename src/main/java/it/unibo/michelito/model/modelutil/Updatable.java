a9889616db8a553b63082543bb71deeaab1fc7fa
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-08 11:39:42 +0100

    in Updatable currentTime now is deltaTime javadoc modified

--- src/main/java/it/unibo/michelito/model/modelutil/Updatable.java
+++ src/main/java/it/unibo/michelito/model/modelutil/Updatable.java
@@ -10,7 +10,7 @@ public interface Updatable extends MazeObject {
      * This method tells the object to update itself in relation to the deltaTime passed.
      * This method could change the state of the maze.
      *
-     * @param deltaTime is the current deltaTime in millisecond.
+     * @param deltaTime is the delta between the last update and this.
      * @param maze is the current {@link Maze}, used to let the {@link MazeObject} change the {@link Maze}.
      */
     void update(long deltaTime, Maze maze);
