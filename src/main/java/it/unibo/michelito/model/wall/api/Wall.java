f10db660ad9022b8a9affbc8ff03461ba9bc390e
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-09 10:16:31 +0100

    javadoc changes to Wall and WallImpl

--- src/main/java/it/unibo/michelito/model/wall/api/Wall.java
+++ src/main/java/it/unibo/michelito/model/wall/api/Wall.java
@@ -3,7 +3,7 @@ package it.unibo.michelito.model.wall.api;
 import it.unibo.michelito.model.modelutil.MazeObject;
 
 /**
- * Interface representing every Wall.
+ * Represents a generic Wall in the game.
  */
 public interface Wall extends MazeObject {
 }
