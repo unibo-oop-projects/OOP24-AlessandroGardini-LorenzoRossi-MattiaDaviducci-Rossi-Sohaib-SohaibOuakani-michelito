1573a3aef8a1dc78c52384e435d04fa0de160b57
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-09 16:28:25 +0100

    javadoc change to ObjectsAdapter

--- src/main/java/it/unibo/michelito/controller/objectsadapter/api/ObjectsAdapter.java
+++ src/main/java/it/unibo/michelito/controller/objectsadapter/api/ObjectsAdapter.java
@@ -1,16 +1,17 @@
 package it.unibo.michelito.controller.objectsadapter.api;
 
+import it.unibo.michelito.controller.levelgenerator.LevelGenerator;
 import it.unibo.michelito.model.modelutil.MazeObject;
 
 import java.util.Set;
 
 /**
- * Interface for an ObjectsAdapter.
+ * Represents an ObjectsAdapter.
  * Adapter for the LevelGenerator.
  */
 public interface ObjectsAdapter {
     /**
-     * This method retrieves a set of {@link MazeObject} from the LevelGenerator.
+     * This method retrieves a set of {@link MazeObject} from the {@link LevelGenerator}.
      *
      * @param level the level to generate the set of {@link MazeObject}s.
      * @return a set of {@link MazeObject} representing the level.
