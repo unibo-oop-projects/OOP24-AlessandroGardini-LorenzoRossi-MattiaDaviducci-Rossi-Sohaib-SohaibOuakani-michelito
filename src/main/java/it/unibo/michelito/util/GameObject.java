c05efb2d2fed30d9d0d309d5a47738731b636cd3
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-09 15:33:50 +0100

    javadoc add to GameObject

--- src/main/java/it/unibo/michelito/util/GameObject.java
+++ src/main/java/it/unibo/michelito/util/GameObject.java
@@ -1,4 +1,12 @@
 package it.unibo.michelito.util;
 
+import it.unibo.michelito.model.maze.api.Maze;
+
+/**
+ * Generic Object of the game, used outside the {@link Maze}.
+ *
+ * @param objectType the {@link ObjectType}.
+ * @param position the {@link Position}.
+ */
 public record GameObject(ObjectType objectType, Position position) {
 }
