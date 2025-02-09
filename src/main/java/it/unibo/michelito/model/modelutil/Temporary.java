782c295651cc03d0ef613f8d2adbbbe9ad40a08b
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2025-02-09 11:08:30 +0100

    javadoc changes to all modelutil interfaces

--- src/main/java/it/unibo/michelito/model/modelutil/Temporary.java
+++ src/main/java/it/unibo/michelito/model/modelutil/Temporary.java
@@ -1,7 +1,7 @@
 package it.unibo.michelito.model.modelutil;
 
 /**
- * Interface that represent all MazeObject that could be destroyed or eliminated.
+ * Represents all {@link MazeObject} that could be destroyed or eliminated.
  */
 public interface Temporary extends MazeObject {
 }
